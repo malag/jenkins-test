8cad66d3b3bebac3bfe51d3cfae8d3078a7ffc95
Mario <malag@comunidad.unam.mx>
2020-12-01 19:52:59 -0600

    Se agregaron scripts de ejecucion y test

---
+++ scripts/run_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --name proyapi -itd --rm -p 5000:5000 malagoiram/proyectoapi:1.1 
---
+++ scripts/test_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl http://localhost:5000
+
+curl http://localhost:5000/saludo/Mario
+
+curl http://localhost:5000/cuadrado/8.0
